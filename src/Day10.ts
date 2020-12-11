61f657b758901635cdd9530783aa19564b8afc37
Adam Haun <adamhaun@gmail.com>
2020-12-11 17:30:17 -0600

    Finished day 10.

--- src/Day10.ts
+++ src/Day10.ts
@@ -28,3 +28,28 @@ numThrees++;
 console.log('Part 1:', numOnes, 'one-jolt differences and', numThrees, 'three-jolt differences');
 console.log('        The product is', numOnes * numThrees);
 
+
+//Part 2: Figure out how many different ways the adapters can be chained together to connect the
+//device and the power source. A brute-force attempt would take way too long, but we can use a
+//recursive algorithm to implement a depth-first search that effectively caches the partial results.
+//We'll need a look-up table for the number of combinations "below" a given adapter.
+type LookupTable = {[index: number]: number};
+
+function lookDown(adapterVoltage: number, adapterLookup: LookupTable): number {
+	if (adapterLookup[adapterVoltage] === undefined) {
+		const compatible = adapters.filter((a: number) => (a < adapterVoltage && a >= adapterVoltage - 3));
+		const directConnection = (adapterVoltage <= 3) ? 1 : 0;
+		
+		var numDownwardCombos = directConnection;
+		for (var a of compatible) {
+			numDownwardCombos += lookDown(a, adapterLookup);
+		}
+		adapterLookup[adapterVoltage] = numDownwardCombos;
+	}
+	
+	//TypeScript doesn't realize that this can't be undefined anymore
+	return adapterLookup[adapterVoltage] as number;
+}
+
+const totalCombos = lookDown(adapters[adapters.length - 1], {} as LookupTable);
+console.log('Part 2: The number of possible adapter combinations is', totalCombos);
